3dc3fe2eb174ab75de422f4f4d24191247826295
Brigite <brigite@correo.com>
2025-05-31 00:54:48 -0500

    subiendo maven

---
+++ Ejercicio1_Maven/Maven_LPII_Ejercicio1/src/main/java/org/cibertec/Persona.java
@@ -0,0 +1,20 @@
+package org.cibertec;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Persona {
+	
+	private String nombre, web; //modificador private es opcional
+	private int edad; //debido a la clase lombok
+	
+	public Persona(String nombre, String web, int edad) {
+		super();
+		this.nombre = nombre;
+		this.web = web;
+		this.edad = edad;
+	}
+	
+}
